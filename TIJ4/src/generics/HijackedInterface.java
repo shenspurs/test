fa392846c27a572d4c4da49f54fef1c9f2659460
Administrator <Administrator@20120803-1741>
2012-09-07 14:55:27 +0800

    first version

---
+++ TIJ4/src/generics/HijackedInterface.java
@@ -0,0 +1,9 @@
+//: generics/HijackedInterface.java
+package generics; /* Added by Eclipse.py */
+// {CompileTimeError} (Won't compile)
+
+class Cat extends ComparablePet {
+  // Error: Comparable cannot be inherited with
+  // different arguments: <Cat> and <Pet>
+  public int compareTo(Cat arg) { return 0; }
+} ///:~
